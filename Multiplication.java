e5df85e5f77b22bca482a44e5d6d4299816d2ab5
varaprasad75844 <152066484+varaprasad75844@users.noreply.github.com>
2024-09-06 15:24:03 +0530

    Create  Multiplication.java

---
+++ Multiplication.java
@@ -0,0 +1,11 @@
+import java.io.*;
+class Multiplication
+{
+public static void main(String args[])
+{
+int a=5;
+int b=10;
+int c=a*b;
+System.out.println("Multiplication of"+a+"and"+b+"is"+c);
+}
+}
